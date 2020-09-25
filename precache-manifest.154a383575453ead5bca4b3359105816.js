27d17c3fc5021d2a0197935692e0cd945aca6fe7
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-09-25 23:34:09 +0530

    Updates

---
+++ precache-manifest.154a383575453ead5bca4b3359105816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3279348b5c1a5c2bf79b4fbe269542a",
+    "url": "/React-Amazon-Clone/index.html"
+  },
+  {
+    "revision": "7a40ef6918ae4d1ec299",
+    "url": "/React-Amazon-Clone/static/css/main.d4f8628e.chunk.css"
+  },
+  {
+    "revision": "356b15c5a61dbe314468",
+    "url": "/React-Amazon-Clone/static/js/2.096454ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Amazon-Clone/static/js/2.096454ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a40ef6918ae4d1ec299",
+    "url": "/React-Amazon-Clone/static/js/main.eb4b620e.chunk.js"
+  },
+  {
+    "revision": "c3ad4c9068927ccbba0d",
+    "url": "/React-Amazon-Clone/static/js/runtime-main.745b8305.js"
+  }
+]);
