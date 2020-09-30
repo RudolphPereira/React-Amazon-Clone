e47fbfd0902609594f3bc2d0101f7953b528e258
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-09-30 10:00:47 +0530

    Added subtotal functionality

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
@@ -40,7 +41,7 @@ function Checkout() {
       </div>
       {basket.length > 0 && (
         <div className="checkout__right">
-          <h1>Subtotal</h1>
+          <Subtotal />
         </div>
       )}
     </div>
---
+++ src/components/LogIn.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LogIn.css";
+
+function LogIn() {
+  return <div></div>;
+}
+
+export default LogIn;
